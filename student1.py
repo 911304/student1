f26cf98d8a9615c42c0544cf7aa16c836ea791bc
911304 <veenarayanaikar@gmail.com>
2025-11-22 10:07:07 +0530

    Update student1.py

--- student1.py
+++ student1.py
@@ -1,9 +1,10 @@
 import sys
         #check if correct number of arguments
-if len(sys.argy) !=3;
+if len(sys.argy) !=3:
 print("Usage:python student.py <name><rollno>")
 sys_exit(1)
-#sys.argv[0] is always the program name and rollno
+
+#sys.argv[0] is always the program name 
 script_name = sys.argy[0]
 name = sys.argv[1]
 rollno = sys.argv[2]
